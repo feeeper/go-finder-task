f6c435d28f63a387153a109d6247dd0e5234510d
feeeper <shirobokov.andrey@gmail.com>
2020-02-25 16:19:09 +0400

    Start exact count of goroutines

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/signal"
 	"regexp"
 	"sync"
 )
@@ -30,38 +31,66 @@ func getOccurances(url string) (int, error) {
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan string, 5)
+	ch := make(chan string)
 	scanner := bufio.NewScanner(os.Stdin)
 	counter := 0
 	mutex := new(sync.Mutex)
+	maxConcurrency := 5
+	urls := []string{}
 
+	// ^C handling
+	interrupts := make(chan os.Signal, 1)
+	signal.Notify(interrupts, os.Interrupt)
 	go func() {
-		for url := range ch {
-			count, err := getOccurances(url)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				fmt.Println(url, "\t", count)
-				mutex.Lock()
-				counter += count
-				mutex.Unlock()
-			}
-
-			wg.Done()
+		for _ = range interrupts {
+			fmt.Println("Total: ", "\t", counter)
+			os.Exit(1)
 		}
 	}()
 
-	for scanner.Scan() {
-		wg.Add(1)
-
-		url := scanner.Text()
-		go func(url string) {
-			ch <- url
-		}(url)
+	// Exit on empty input
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) != 0 {
+		fmt.Println("Invalid input")
+		os.Exit(1)
 	}
 
-	wg.Wait()
-	close(ch)
+	// Reading from stdin
+	for scanner.Scan() {
+		url := scanner.Text()
+		urls = append(urls, url)
+	}
 
+	// Total count of goroutines should be equal or less then total count of urls
+	concurrency := maxConcurrency
+	if len(urls) < maxConcurrency {
+		concurrency = len(urls)
+	}
+
+	// Get occurances and update total counter
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			for url := range ch {
+				count, err := getOccurances(url)
+				if err != nil {
+					fmt.Println(err.Error())
+				} else {
+					fmt.Println(url, "\t", count)
+					mutex.Lock()
+					counter += count
+					mutex.Unlock()
+				}
+			}
+			wg.Done()
+		}()
+	}
+
+	for _, url := range urls {
+		ch <- url
+	}
+
+	close(ch)
+	wg.Wait()
 	fmt.Println("Total: ", "\t", counter)
 }
